d6e7c690c1c11f76e6084fe9dd9a46ecbbe27e35
Muhammad-Hassan-Farid <hassan.khan.wazir12@gmail.com>
2025-09-14 15:36:43 +0500

    Requirements setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical_Chatbot",
+    version="0.1.0",
+    author="Muhammad Hassan Farid",
+    author_email="hassan.khan.wazir12@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
